26973633c5e360d3902f2db11ab5ca21b76f6754
Salvatore Butera <salvatore.butera@nttdata.com>
2022-04-12 15:27:40 +0200

    find bug

--- script.js
+++ script.js
@@ -1,25 +1,71 @@
 import {chars} from './chars.js';
 
+//VARIABLES 
+var bug;
+var circle;
+var myEvent;
+var intervalAsci;
+var width;
+//ONLOAD
 window.addEventListener("load",()=>{
+    width = window.innerWidth;
     document.getElementById("asci-container").style.fontSize = "calc(100vw / 60) "
     var asciString = ""
-        for (var i = 0; i<500; i++) {
-            for (var j = 0; j < chars.length; j++){
-                asciString = asciString.concat(chars[j]);
-            }
+    for (var i = 0; i<300; i++) {
+        for (var j = 0; j < chars.length; j++){
+            asciString = asciString.concat(chars[j]);
         }
-    setInterval(()=>{
+           
+    }
+    asciString = asciString.concat('<i class="fa fa-bug" aria-hidden="true"></i>');
+    intervalAsci = setInterval(()=>{
         let toPut = asciString[0];
-        asciString = asciString.slice(1);
-        if(getRandom(5) == getRandom(5)){
-            toPut = getRandom(2);
-        }  
-        asciString = asciString.concat(toPut);
-        document.getElementById("asci-container").style.color = "#10c5" + getRandom(9) + getRandom(9); 
-        document.getElementById("asci-container").innerHTML = asciString;
+        if(toPut == "0" || toPut == "1") {
+            asciString = asciString.slice(1);
+            if(getRandom(5) == getRandom(5)){
+                toPut = getRandom(2);
+            }  
+            asciString = asciString.concat(toPut);
+            document.getElementById("asci-container").innerHTML = asciString;
+        }
     },75)
-})
+    setTimeout(()=>{
+        window.addEventListener('mousemove', circleFollow);
+    },250);
+});
 
+window.addEventListener("bugfound", () => nextStep());
+
+//EVENT LISTENER FUNCTIONS\
+var circleFollow = (e) =>{
+    var x = e.pageX;
+    var y = e.pageY;
+    circle = document.getElementById("circle")
+    circle.style.left = x + "px"; ;
+    circle.style.top = y + "px";
+    bug =  document.querySelector("i")
+    if(checkNearBug(e)){
+        console.log("FOUND!");
+        window.removeEventListener("mousemove",circleFollow);
+        const bugFound = new Event('bugfound',{bubbles:true,cancelable: true,composed:true})
+        dispatchEvent(bugFound);
+    }
+}
+
+//FUNCTIONS
+function checkNearBug(e) {
+    var range = width / 13;
+    return bug.getBoundingClientRect().x >= e.pageX - range && 
+    bug.getBoundingClientRect().x <=  e.pageX + range &&
+    bug.getBoundingClientRect().y >= e.pageY - range &&
+    bug.getBoundingClientRect().y <= e.pageY +range;     
+}
 function getRandom(max) {
     return Math.floor(Math.random() * max);
-  }
+}
+
+function nextStep() {
+    clearInterval(intervalAsci);
+    document.querySelector("body").classList.add("after-found");
+    document.getElementById("circle").classList.add("opacity-0");
+}
